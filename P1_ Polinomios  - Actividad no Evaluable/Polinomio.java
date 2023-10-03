5d350c0c700b4e60de79339a20c297082711d68a
K4chann <124625996+K4chann@users.noreply.github.com>
2023-10-03 11:54:04 +0100

    Add files via upload

---
+++ P1_ Polinomios  - Actividad no Evaluable/Polinomio.java
@@ -0,0 +1,154 @@
+import java.util.Arrays;
+
+
+public class Polinomio {
+    //Atributos
+    private int[] coefficients;
+    private int grade;
+    
+    //Métodos
+    //Constructor por omisión
+    public Polinomio() {
+        coefficients = new int[]{0};
+        grade = 0;
+    }
+    
+    //Constructor al que se le pasa v de int
+    public Polinomio(int[] arg) {
+        coefficients = Arrays.copyOf(arg, arg.length);
+        this.calcGrade(coefficients);
+    }
+    
+    //Método que devuelve el grado
+    public int grado() {
+        return grade;
+    }
+    
+    //Método que devuelve el coeficiente en la posición dada
+    public int coeficiente(int index) {
+        return (index >= 0 && index <= grade) ? coefficients[index] : 0;
+    }
+
+    //Método que cambia el valor del coeficiente en la posición dada    
+    public void coeficiente(int index, int value) {
+        
+        if (index > grade) {
+            coefficients = Arrays.copyOf(coefficients, index + 1);
+            coefficients[index] = value; 
+        }
+        
+        coefficients[index] = value;
+        
+        this.calcGrade(coefficients);
+    }
+    
+    //Método que devuelve los coeficientes del polinomio
+    public int[] coeficientes() {
+        return Arrays.copyOf(coefficients, this.grado() + 1);
+    }
+    
+    @Override
+    public String toString() {
+        String rs = new String();
+        
+        for (int i = coefficients.length - 1; i >= 0; i--) {
+            int item = coefficients[i];
+        
+            if (item != 0) {
+                
+                if (i > 0) {
+                    
+                    rs += (item < 0) ? "-" : "+";
+                    
+                    rs += ((Math.abs(item) != 1) ? Math.abs(item) + "x" : "x");
+                    
+                    if (i > 1) {
+                        rs += ("^" + i);
+                    }
+                    
+                } else {
+                    rs += (item < 0) ? "-" : "+";
+                    rs += Math.abs(item);
+                }
+            }
+        }
+        
+        if (rs.length() == 0) {
+            return "0";
+        } else if (rs.length() == 1 || ((Character) rs.charAt(0)).equals('-')) {
+            return rs;
+        } else {
+            return rs.substring(1, rs.length());
+        }
+    }
+    
+    //Método que devuelve el valor del polinomio donde x = v
+    public float valor(float value) {
+        float rs = 0F;
+        
+        for (int i = 0; i <= grade; i++) {
+            rs += coefficients[i] * Math.pow(value, i);
+        }
+        
+        return rs;
+    }
+    
+    //Método que suma dos polinomios
+    public Polinomio suma(Polinomio pol) {
+        int[] polCoeff = pol.coeficientes();
+        
+        int maxLen = Math.max(polCoeff.length, coefficients.length);
+        int minLen = Math.min(polCoeff.length, coefficients.length);
+        
+        int[] rs = Arrays.copyOf(
+            (coefficients.length == maxLen) ? coefficients : polCoeff,
+            maxLen
+        );
+        
+        for (int i = 0; i < minLen; i++) {
+            rs[i] = coefficients[i] + polCoeff[i];
+        }
+        
+        return new Polinomio(rs);
+    }
+    
+    //Método que resta dos polinomios
+    public Polinomio resta(Polinomio pol) {
+        int[] polCoeff = pol.coeficientes();
+        
+        int maxLen = Math.max(polCoeff.length, coefficients.length);
+        int[] rs = new int[maxLen];
+        
+        int[] actualPol = Arrays.copyOf(coefficients, maxLen);
+        int[] otherPol = Arrays.copyOf(polCoeff, maxLen);
+        
+        for (int i = 0; i < maxLen; i++) {
+            rs[i] = actualPol[i] - otherPol[i];
+        }
+        
+        System.out.println(Arrays.toString(rs));
+        return new Polinomio(rs);
+    }
+    
+    private void viewPolynomial() {
+        for (int i = 0; i < grade; i++) {
+            System.out.print(coefficients[i] + " ");
+        }
+    }
+    
+    //Método que calcula el valor del polinomio
+    private void calcGrade(int[] pol) {
+        grade = pol.length - 1;
+
+        for (int i = pol.length - 1; i >= 0; i--) {
+            if (pol[i] != 0) {
+                break;
+            }
+            grade--;
+        }
+        
+        if (grade < 0) {
+            grade = 0;
+        }
+    }
+}
---
+++ P1_ Polinomios  - Actividad no Evaluable/UsePolynomials.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+
+public class UsePolynomials {
+    public static void main(String[] args) {
+        // Usa éste módulo para ejecutar el código y probar las funcionalidades
+        // de la clase Polinomio
+    }
+}
